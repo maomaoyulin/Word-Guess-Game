d1e41d635fc19898fb790a363667f03cd902ac01
maomaoyulin <maoyulin527@yahoo.com>
2018-04-06 21:41:12 -0500

    Apr,6th

---
+++ assets/javascript/game.js
@@ -0,0 +1,71 @@
+
+$(document).ready(function() {
+
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghijklmnopqrstuvwxyz";
+    var carBrands, wins, remaining, guessedLetter;
+
+    carBrands = ["FORD", "TOYOTA", "CHEVROLET", "HONDA", "NISSAN", "JEEP", "HYUNDAI", "KIA", "SUBARU", "GMC"];
+    wins = 0;
+    remaining = 13;
+    guessedLetter = [];
+
+    var computerOption = Math.floor(Math.random() * carBrands.length);
+    var currentComputerOption = carBrands[computerOption];
+
+    var elRemaining = document.getElementById("remaining");
+    elRemaining.textContent = remaining;
+
+    console.log(currentComputerOption);
+    console.log(currentComputerOption.length);
+
+    var repeatString = "";
+    function repeatStringNumTimes(string, times) {
+        
+        while(times > 0) {
+            repeatString += string;
+            times--;
+        };
+        return repeatString;
+    }
+
+    console.log(repeatStringNumTimes("_ ", currentComputerOption.length));
+
+    var current = document.getElementById("currentWord");
+    current.textContent = repeatStringNumTimes("_ ", currentComputerOption.length);
+
+    document.onkeyup = function(event) {
+
+        var userGuess = event.key;
+        console.log(userGuess);
+
+        var upperUserGuess = userGuess.toUpperCase();
+        console.log(upperUserGuess);
+
+        var rightGuess = currentComputerOption.indexOf(upperUserGuess);
+
+        if(rightGuess !== -1) {
+            remaining--;
+            elRemaining.textContent = remaining;
+        }
+        
+        var indices = [];
+
+        for(var i=0; i<currentComputerOption.length;i++) {
+            if (currentComputerOption[i] === upperUserGuess) indices.push(i);
+        };
+        console.log(indices);
+
+        for(var j=0; j<currentComputerOption.length;j++) {
+            while (currentComputerOption[j] === upperUserGuess) {
+                repeatStringNumTimes("_ ", currentComputerOption.length)[j] = upperUserGuess;
+            };
+        };
+
+        
+
+    }
+});
+
+
+// var elUserGuss = document.getElementById("guessedLetter");
+// elUserGuss.textContent = userGuess;
